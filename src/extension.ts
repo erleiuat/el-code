44b2cdd051e347a2ae1c004ab2808bcef391b34b
Elia <elia.reutlinger@yoo.digital>
2021-12-12 20:04:34 +0100

    aaa

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,21 @@
+import * as elCode from './el-code/el-code'
 import * as vs from 'vscode'
-import * as elCode from './el-code/el-Code'
 
-export function activate(context: vs.ExtensionContext) {
+export function activate (context: vs.ExtensionContext) {
 
-	context.subscriptions.push(
-		vs.commands.registerCommand(
-			'el-code.quickConsole',
-			elCode.quickConsole
-		)
-	)
-
-	context.subscriptions.push(
-		vs.commands.registerCommand(
-			'el-code.commentSelection',
-			elCode.commentSelection
-		)
-	)
+  context.subscriptions.push(
+    vs.commands.registerCommand(
+      'el-code.quickConsole',
+      elCode.quickConsole
+    )
+  )
+  context.subscriptions.push(
+    vs.commands.registerCommand(
+      'el-code.commentSelection',
+      elCode.commentSelection
+    )
+  )
 
 }
 
-export function deactivate() { }
+export function deactivate () { }
